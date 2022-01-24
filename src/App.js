2206b2d4ce44aa9cd5624fb546fde169ac134c97
Kep <KepCreations@gmail.com>
2022-01-24 17:36:51 -0500

    Add Total to cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart } from './actions';
-import ProductDetails from './ProductDetails';
 import Cart from './Cart';
 import './App.css';
 
@@ -9,28 +8,19 @@ function App() {
 	const products = useSelector((store) => store.products);
 	const dispatch = useDispatch();
 
-	//   <Product
-	//   key={p}
-	//   id={p}
-	//   name={products[p].name}
-	//   price={products[p].price}
-	//   description={products[p].description}
-	//   image={products[p].image_url}
-	// />
-
 	const add = (id, price) => {
 		dispatch(addToCart(id, price));
 	};
 
-	const productList = Object.keys(products).map((p) => (
-		<div className="col-md-3 mb-3" key={p}>
+	const productList = Object.keys(products).map((id) => (
+		<div className="col-md-3 mb-3" key={id}>
 			<div className="card">
 				<div className="card-body">
-					<h4 className="card-title text-center">{products[p].name}</h4>
-					<p>${products[p].price}</p>
+					<h4 className="card-title text-center">{products[id].name}</h4>
+					<p>${products[id].price}</p>
 					<button
 						className="btn btn-primary"
-						onClick={() => add(p, [ p ].price)}
+						onClick={() => add(id, products[id].price)}
 					>
 						Add to Cart
 					</button>
@@ -38,7 +28,7 @@ function App() {
 			</div>
 		</div>
 	));
-	console.log(productList);
+
 	return (
 		<div className="App container">
 			<h1>Shoply</h1>
--- src/Cart.js
+++ src/Cart.js
@@ -5,13 +5,16 @@ import './Cart.css';
 const Cart = () => {
 	const items = useSelector((store) => store.cart.items);
 	const products = useSelector((store) => store.products);
-	console.log(items);
+	const total = useSelector((store) => store.cart.total);
 
 	const itemsList = Object.keys(items).map((i) => (
 		<tr key={i}>
 			<td className="text-center align-middle">{products[i].name}</td>
 			<td className="text-center align-middle">${products[i].price}</td>
 			<td className="text-center align-middle">{items[i]}</td>
+			<td className="text-center align-middle">
+				{items[i] * products[i].price}
+			</td>
 		</tr>
 	));
 
@@ -24,9 +27,22 @@ const Cart = () => {
 						<th>Item Name</th>
 						<th>Item Price</th>
 						<th>Item Quantity</th>
+						<th>Sub-Total</th>
 					</tr>
 				</thead>
-				<tbody>{itemsList}</tbody>
+				<tbody>
+					{itemsList}
+					<tr className="text-center align-center">
+						<td />
+						<td />
+						<td className="text-right">
+							<strong>Total</strong>
+						</td>
+						<td>
+							<strong>${total}</strong>
+						</td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
 	);
